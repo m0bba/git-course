6b9af5e60a746d493f5e51799731b0d3ea06acf7
m0bba <igor0071@inbox.lv>
2022-11-13 18:58:22 +0200

    record collection

--- codewars.js
+++ codewars.js
@@ -1,14 +1,41 @@
-function checkObj(obj, checkProp) {
-  // Only change code below this line
-  if (obj.hasOwnProperty(checkProp)) {
-      return obj[checkProp]
+// Setup
+const recordCollection = {
+  2548: {
+    albumTitle: 'Slippery When Wet',
+    artist: 'Bon Jovi',
+    tracks: ['Let It Rock', 'You Give Love a Bad Name']
+  },
+  2468: {
+    albumTitle: '1999',
+    artist: 'Prince',
+    tracks: ['1999', 'Little Red Corvette']
+  },
+  1245: {
+    artist: 'Robert Palmer',
+    tracks: []
+  },
+  5439: {
+    albumTitle: 'ABBA Gold'
   }
-  else {
-    return "Not Found"
+};
+
+// Only change code below this line
+function updateRecords(records, id, prop, value) {
+  if(prop != 'tracks' && value != "") {
+    records[id][prop] = value
+  } 
+  else if(prop == 'tracks' && !records[id].hasOwnProperty('tracks')) {
+     records[id][prop] = [value]
+  } else if (prop == 'tracks' && value != "") {
+    records[id][prop].push(value)
   }
-  // Only change code above this line
+    else if (value == "") {
+    delete records[id][prop]
+  }
+
+  
+return records;
 }
-const objekt = {
-  gift: "pony", pet: "kitten", bed: "sleigh"
-}
-console.log(checkObj(objekt, "gift"))
+
+console.log(updateRecords(recordCollection, 1245, 'tracks', 'Addicted to Love'));
+console.log(recordCollection[2468].hasOwnProperty('track'))
