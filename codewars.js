1939fdc0c7b39cd931cfadfbd4858d98df1e92c3
m0bba <igor0071@inbox.lv>
2022-11-15 03:06:24 +0200

    loop

--- codewars.js
+++ codewars.js
@@ -1,41 +1,8 @@
 // Setup
-const recordCollection = {
-  2548: {
-    albumTitle: 'Slippery When Wet',
-    artist: 'Bon Jovi',
-    tracks: ['Let It Rock', 'You Give Love a Bad Name']
-  },
-  2468: {
-    albumTitle: '1999',
-    artist: 'Prince',
-    tracks: ['1999', 'Little Red Corvette']
-  },
-  1245: {
-    artist: 'Robert Palmer',
-    tracks: []
-  },
-  5439: {
-    albumTitle: 'ABBA Gold'
-  }
-};
-
-// Only change code below this line
-function updateRecords(records, id, prop, value) {
-  if(prop != 'tracks' && value != "") {
-    records[id][prop] = value
-  } 
-  else if(prop == 'tracks' && !records[id].hasOwnProperty('tracks')) {
-     records[id][prop] = [value]
-  } else if (prop == 'tracks' && value != "") {
-    records[id][prop].push(value)
-  }
-    else if (value == "") {
-    delete records[id][prop]
-  }
-
-  
-return records;
+const myArray = [];
+let i = 5
+while (i > 0) {
+  myArray.push(i)
+  i--
 }
-
-console.log(updateRecords(recordCollection, 1245, 'tracks', 'Addicted to Love'));
-console.log(recordCollection[2468].hasOwnProperty('track'))
+console.log(myArray)// Only change code below this line
