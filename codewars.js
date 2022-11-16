8637a7f3a39f5091b79411dd1b55f3180912334b
m0bba <igor0071@inbox.lv>
2022-11-16 06:02:00 +0200

    q

--- codewars.js
+++ codewars.js
@@ -1,8 +1,41 @@
 // Setup
-const myArray = [];
-let i = 5
-while (i > 0) {
-  myArray.push(i)
-  i--
+const contacts = [
+  {
+    firstName: "Akira",
+    lastName: "Laine",
+    number: "0543236543",
+    likes: ["Pizza", "Coding", "Brownie Points"],
+  },
+  {
+    firstName: "Harry",
+    lastName: "Potter",
+    number: "0994372684",
+    likes: ["Hogwarts", "Magic", "Hagrid"],
+  },
+  {
+    firstName: "Sherlock",
+    lastName: "Holmes",
+    number: "0487345643",
+    likes: ["Intriguing Cases", "Violin"],
+  },
+  {
+    firstName: "Kristian",
+    lastName: "Vos",
+    number: "unknown",
+    likes: ["JavaScript", "Gaming", "Foxes"],
+  },
+];
+
+function lookUpProfile(name, prop) {
+  // Only change code below this line
+for(let i = 0; i < contacts.length; i++){
+  if(contacts[i].hasOwnProperty(prop)) {
+    if(contacts[i]["firstName"] === name) {
+      console.log(contacts[i][prop])
+    } else return console.log("-")
+  }
 }
-console.log(myArray)// Only change code below this line
+}
+
+lookUpProfile("Kristian", "lastName");
+
